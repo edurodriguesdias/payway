714f66da786e2953bde22634863d79a14eea5de8
Eduardo Dias <edu_rodriguesdias@hotmail.com>
2026-03-29 00:45:16 -0300

    fix: remove useless constructor exception

--- src/main/java/com/example/payway/common/exception/BusinessException.java
+++ src/main/java/com/example/payway/common/exception/BusinessException.java
@@ -1,12 +1,7 @@
 package com.example.payway.common.exception;
 
 public class BusinessException extends RuntimeException {
-
-    public BusinessException(String message) {
-        super(message);
-    }
-
-    public BusinessException(String message, Throwable cause) {
-        super(message, cause);
-    }
+  public BusinessException(String message) {
+      super(message);
+  }
 }
--- src/main/java/com/example/payway/common/exception/NotFoundException.java
+++ src/main/java/com/example/payway/common/exception/NotFoundException.java
@@ -1,12 +1,7 @@
 package com.example.payway.common.exception;
 
 public class NotFoundException extends RuntimeException {
-
     public NotFoundException(String message) {
         super(message);
-    }
-
-    public NotFoundException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
